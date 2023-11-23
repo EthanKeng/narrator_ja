9753dd659f9deadfee885984978130e7ae6baf04
YK <36765774+EthanKeng@users.noreply.github.com>
2023-11-23 18:18:23 +0900

    add Japanese model "eleven_multilingual_v2" and edit gpt prompt to output Japanese

--- narrator.py
+++ narrator.py
@@ -3,7 +3,6 @@ from openai import OpenAI
 import base64
 import json
 import time
-import simpleaudio as sa
 import errno
 from elevenlabs import generate, play, set_api_key, voices
 
@@ -25,7 +24,7 @@ def encode_image(image_path):
 
 
 def play_audio(text):
-    audio = generate(text, voice=os.environ.get("ELEVENLABS_VOICE_ID"))
+    audio = generate(text,  model="eleven_multilingual_v2",voice=os.environ.get("ELEVENLABS_VOICE_ID"))
 
     unique_id = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8").rstrip("=")
     dir_path = os.path.join("narration", unique_id)
@@ -43,7 +42,7 @@ def generate_new_line(base64_image):
         {
             "role": "user",
             "content": [
-                {"type": "text", "text": "Describe this image"},
+                {"type": "text", "text": "Describe this image in Japanese"},
                 {
                     "type": "image_url",
                     "image_url": f"data:image/jpeg;base64,{base64_image}",
@@ -61,7 +60,7 @@ def analyze_image(base64_image, script):
                 "role": "system",
                 "content": """
                 You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.
-                Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!
+                Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it! 
                 """,
             },
         ]
